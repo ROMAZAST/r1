ac78c195af2139bf87945a5c37dc92cd2640a3f5
Roman <100799424+ROMAZAST@users.noreply.github.com>
2025-07-09 19:34:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    frame = cv2.resize(frame, (1280, 720))  # Задаємо потрібну якість
+    frame = cv2.resize(frame, (480, 320))  # Задаємо потрібну якість
     cv2.imshow("Live Feed", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
